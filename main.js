9f9f78feb4988033a0831a299d36a33d7bee165c
alex-alk <alex.leo06@yahoo.ro>
2025-06-18 22:17:47 +0300

    update

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const bindings = {};
 
 export function init(component) {
-    const all = component.querySelectorAll('[v-text], [v-bind\\:src], [v-if], [v-for]');
+    const all = component.querySelectorAll('[v-text], [v-bind\\:src], [v-if], [v-for], [v-on\\:click], [v-on\\:mouseover]');
 
     for (const el of all) {
         const directives = [
             { attr: 'v-text', key: el.getAttribute('v-text') },
             { attr: 'v-bind:src', key: el.getAttribute('v-bind:src') },
             { attr: 'v-if', key: el.getAttribute('v-if') },
-            { attr: 'v-for', key: el.getAttribute('v-for') }
+            { attr: 'v-for', key: el.getAttribute('v-for') },
+            { attr: 'v-on:click', key: el.getAttribute('v-on:click') },
+            { attr: 'v-on:mouseover', key: el.getAttribute('v-on:mouseover') }
         ];
 
         for (const { attr, key } of directives) {
@@ -38,71 +40,101 @@ export const state = new Proxy({},
     {
         set(target, variableName, value) {
             target[variableName] = value;
-
             // Update all bound elements
-            if (bindings[variableName]['v-text']) {
-                for (const el of bindings[variableName]['v-text']) {
-                    el.textContent = value;
-                }
-            }
+            if (bindings[variableName]) {
 
-            if (bindings[variableName]['v-bind:src']) {
-                for (const el of bindings[variableName]['v-bind:src']) {
-                    el.src = value;
-                }
-            }
+                if (bindings[variableName]['v-for']) {
+                    for (const el of bindings[variableName]['v-for']) {
+                        const elid = el.getAttribute('elid');
 
-            if (bindings[variableName]['v-if']) {
-                for (const el of bindings[variableName]['v-if']) {
-                    if (!value) {
-                        el.style.display = 'none';
-                    } else {
-                        console.log(el)
-                        el.style.removeProperty('display')
-                    }
-                    // get the adjacent element
-                    const next = el.nextElementSibling;
-                    if (next.hasAttribute('v-else')) {
-                        if (value) {
-                            next.style.display = 'none';
+                        const attribute = el.getAttribute('v-for');
+                        const array = attribute.split('::')
+                        const size = array.length
+
+                        if (!elid) {
+                            // value will be data array
+                            let i = 0
+                            for (const valueText of value) {
+                                const clone = el.cloneNode(true)
+
+                                if (size === 1){
+                                    clone.textContent = valueText
+                                }
+                                if (size === 2){
+                                    clone.textContent = valueText[array[size - 1]]
+                                }
+                                clone.setAttribute('elid', i)
+                                i++
+
+                                for (const attr of clone.attributes) {
+                                    if (attr.name.startsWith('v-on:')) {
+                                        const eventName = attr.name.split(':')[1];
+                                        const handlerName = attr.value;
+               
+                                        if (handlerName && state[handlerName]) {
+                                            clone.addEventListener(eventName, state[handlerName]);
+                                        }
+                                    }
+                                }
+                                el.after(clone);
+                            }
+                            el.remove()
                         } else {
-                            next.style.removeProperty('display')
+
                         }
+                        // ia elementul
+                        // daca nu are elid
+                        // genereaza
+                        // daca parintele are elemente cu elid, compara cu array
+                        // syncronizeaza
+
                     }
                 }
-            }
 
-            if (bindings[variableName]['v-for']) {
-                for (const el of bindings[variableName]['v-for']) {
-                    const elid = el.getAttribute('elid');
-
-                    const attribute = el.getAttribute('v-for');
-                    const array = attribute.split('::')
-                    const size = array.length
-
-                    if (!elid) {
-                        // value will be data array
-                        for (const valueText of value) {
-                            const clone = el.cloneNode(true)
-
-                            if (size === 1){
-                                clone.textContent = valueText
-                            }
-                            if (size === 2){
-                                clone.textContent = valueText[array[size - 1]]
-                            }
-                            el.after(clone);
-                        }
-                        el.remove()
-                    } else {
-
+                if (bindings[variableName]['v-text']) {
+                    for (const el of bindings[variableName]['v-text']) {
+                        el.textContent = value;
                     }
-                    // ia elementul
-                    // daca nu are elid
-                    // genereaza
-                    // daca parintele are elemente cu elid, compara cu array
-                    // syncronizeaza
+                }
 
+                if (bindings[variableName]['v-bind:src']) {
+                    for (const el of bindings[variableName]['v-bind:src']) {
+                        el.src = value;
+                    }
+                }
+
+                if (bindings[variableName]['v-on:click']) {
+                    for (const el of bindings[variableName]['v-on:click']) {
+                        el.onclick = value;
+                    }
+                }
+
+                // if (bindings[variableName]['v-on:mouseover']) {
+                    
+                //     for (const el of bindings[variableName]['v-on:mouseover']) {
+                //         console.log(el, value)
+                //         el.onmouseover = value
+                //     }
+                // }
+
+                if (bindings[variableName]['v-if']) {
+                    for (const el of bindings[variableName]['v-if']) {
+                        if (!value) {
+                            el.style.display = 'none';
+                        } else {
+                            console.log(el)
+                            el.style.removeProperty('display')
+                        }
+                        // get the adjacent element
+                        const next = el.nextElementSibling;
+                        if (next.hasAttribute('v-else')) {
+                            if (value) {
+                                next.style.display = 'none';
+                            } else {
+                                next.style.removeProperty('display')
+                            }
+                        }
+                    }
                 }
             }
 
@@ -113,75 +145,14 @@ export const state = new Proxy({},
 
 export function ref(variableName, value) {
   state[variableName] = value;
-  return state[variableName];
+  return {
+    get value() {
+      return state[variableName];
+    },
+    set value(val) {
+      state[variableName] = val;
+    }
+  };
 }
-
-// const array = ['a', 'b']
-
-// state.text = text
-// state.array = array
-
-// setInterval(function () {
-//     state.text++
-
-//     console.log(state.text, text)
-// }, 1000);
-
-// foreach el
-// if attr
-// process each
-//
-
-
-
-// function div(text) {
-//   const el = document.createElement('div');
-//   el.textContent = text;
-//   el.a = function(child) {
-//     this.appendChild(child);
-//     return this; // return parent for chaining
-//   };
-//   return el;
-// }
-
-// function ul(texts) {
-//   const el = document.createElement('ul');
-
-//   for (const text of texts) {
-//     el.append(li(text))
-//   }
-
-//   el.a = function(child) {
-//     this.appendChild(child);
-//     return this; // return parent for chaining
-//   };
-//   return el;
-// }
-
-// function li(text) {
-//   const el = document.createElement('li');
-//   el.textContent = text;
-//   el.a = function(child) {
-//     this.appendChild(child);
-//     return this; // return parent for chaining
-//   };
-//   return el;
-// }
-
-// function updateUl(ulElement, texts) {
-//   ulElement.innerHTML = ''; // clear current children
-//   for (const text of texts) {
-//     ulElement.appendChild(li(text));
-//   }
-// }
-
-// setInterval(function () {
-//     texts.pop()
-//     updateUl(ule, texts);
-//     console.log(texts)
-// }, 1000);
-
-
-//export default component;
 
 
